208c9879b574689b90e49aa11609c64711683ca8
Sarychev Vladmir <svmyhome@mail.ru>
2022-04-05 22:40:08 +0300

    The following changes added: - a new way to check element exist or not exist

--- src/main/java/EnabledSelectedDisplayed/EnabSelecDispl.java
+++ src/main/java/EnabledSelectedDisplayed/EnabSelecDispl.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
-/** 56 isDisplayed, isSelected, isEnabled, existing element on screen*/
+/** 56 - 57 isDisplayed, isSelected, isEnabled, existing element on screen*/
 public class EnabSelecDispl {
     static WebDriver driver;
 
