819c468c33b58bf69f9a3e335a80035d314ade9b
vladimir.sarychev <svmyhome@mail.ru>
2022-04-07 21:04:57 +0300

    The following changes:
    -  Added POM and PageFactory models

--- src/main/java/PFactory/MainClass.java
+++ src/main/java/PFactory/MainClass.java
@@ -1,3 +1,5 @@
+package PFactory;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
--- src/main/java/POM/SignUpPage.java
+++ src/main/java/POM/SignUpPage.java
@@ -1,7 +1,7 @@
+package POM;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -19,27 +19,16 @@ public class SignUpPage {
         this.driver = driver;
     }
 
-    @FindBy(xpath = "//input[@id='email']")
-    private WebElement emailField;
-    @FindBy(xpath = "//span[@class='text-mono text-gray-mktg']")
-    private WebElement textAdventure;
-    @FindBy(xpath = "//button[contains(@class,'js-continue-button signup-continue-button')]")
-    private WebElement emailButton; //TODO create class for fille field abd before check for disabling
-    @FindBy(xpath = "//input[@id='password']")
-    private WebElement passField;
-    @FindBy(xpath = "//button[@data-continue-to='username-container']")
-    private WebElement passButton;
-    @FindBy(xpath = "//input[@id='login']")
-    private WebElement nameField;
-    @FindBy(xpath = "//input[@id='login']")
-    private WebElement nameButton;
-    @FindBy(xpath = "//label[@for='opt_in']")
-    private WebElement textNotification;
-    @FindBy(xpath = "//input[@name='opt_in']")
-    private WebElement NotificationField;
-    @FindBy(xpath = "//button[@data-optimizely-event='click.signup_continue.opt-in']")
-    private WebElement NotificationButton;
-
+    private By emailField = By.xpath("//input[@id='email']");
+    private By textAdventure = By.xpath("//span[@class='text-mono text-gray-mktg']");
+    private By emailButton = By.xpath("//button[contains(@class,'js-continue-button signup-continue-button')]"); //TODO create class for fille field abd before check for disabling
+    private By passField = By.xpath("//input[@id='password']");
+    private By passButton = By.xpath("//button[@data-continue-to='username-container']");
+    private By nameField = By.xpath("//input[@id='login']");
+    private By nameButton = By.xpath("//input[@id='login']");
+    private By textNotification = By.xpath("//label[@for='opt_in']");
+    private By NotificationField = By.xpath("//input[@name='opt_in']");
+    private By NotificationButton = By.xpath("//button[@data-optimizely-event='click.signup_continue.opt-in']");
 
     public String getAdventure() {
         try {
@@ -47,15 +36,15 @@ public class SignUpPage {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return textAdventure.getText();
+        return driver.findElement(textAdventure).getText();
     }
 
     public SignUpPage typeEmailField(String email) {
-        emailField.sendKeys(email);
+        driver.findElement(emailField).sendKeys(email);
         return this;
     }
 
-    public SignUpPage waitField(WebElement field) {
+    public SignUpPage waitField(By field) {
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(field));
         return this;
@@ -66,20 +55,20 @@ public class SignUpPage {
 //        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 //        wait.until(ExpectedConditions.elementToBeClickable(emailButton));
         waitField(emailButton);
-        emailButton.click();
+        driver.findElement(emailButton).click();
         return new SignUpPage(driver);
     }
 
     public SignUpPage typePassField(String email, String pass) {
         this.clickEmailButton(email);
-        passField.sendKeys(pass);
+        driver.findElement(passField).sendKeys(pass);
         return this;
     }
 
     public SignUpPage clickpassButton(String email, String pass) {
         this.typePassField(email, pass);
         waitField(passButton);
-        passButton.click();
+        driver.findElement(passButton).click();
         return new SignUpPage(driver);
     }
 
