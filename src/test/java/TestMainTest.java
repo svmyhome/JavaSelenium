5b21dd5c2bf9e104605f0434f0989941b94c6683
Sarychev Vladmir <svmyhome@mail.ru>
2022-04-09 22:42:03 +0300

    The following changes added: -  AssertArray was added

--- src/test/java/TestMainTest.java
+++ src/test/java/TestMainTest.java
@@ -21,27 +21,46 @@ public class TestMainTest {
     public void assertFalseMethod2() {
         Assertions.assertFalse(2 == 2, "Value are not false!");
     }
+
     @Test
     public void assertNullMethod3() {
         Assertions.assertNull(2, "Value are not null!");
     }
+
     @Test
     public void assertNotNullMethod4() {
-        Assertions.assertNotNull(2,"Value are null!");
+        Assertions.assertNotNull(2, "Value are null!");
     }
+
     @Test
     public void assertEqualsMethod5() {
         Assertions.assertEquals(20, 10 + 5, "Value are not equals!");
     }
+
     @Test
     public void assertNotEqualsMethod6() {
         Assertions.assertNotEquals(20, 10 + 5, "Value are equals!");
+    }
+
+    @Test
+    public void assertArraysMethod7True() {
+        int[] expected = {1, 3, 2, 6};
+        int[] actual = {1, 3, 2, 6};
+        Assertions.assertArrayEquals(expected, actual, "Arrays are not equals");
+    }
+
+    @Test
+    public void assertArraysMethod7False() {
+        int[] expected = {1, 3, 2, 6};
+        int[] actual = {1, 2, 2, 6};
+        Assertions.assertArrayEquals(expected, actual, "Arrays are not equals");
     }
 
     @AfterEach
     public void tearDown() {
         System.out.println("each method");
     }
+
     @AfterAll
     public static void afterAllMethod() {
         System.out.println("All class");
