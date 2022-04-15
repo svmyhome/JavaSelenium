3b6ca88b5262904a58ab4642cb4bd769a136fa81
Sarychev Vladmir <svmyhome@mail.ru>
2022-04-15 10:13:49 +0300

    The following changes added: - added IframeTest.java

---
+++ src/test/java/IframeTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class IframeTest {
+
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+    @BeforeAll
+    static void setUpAll() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // принудительное ожидание
+        driver.manage().window().maximize(); // установка максимального размера экрана
+        driver.get("https://www.random.org/");
+    }
+    @Test //TODo WebDriver, технологии, настройка – Step 3 – Stepik.mp
+    void iframetest() {
+//        driver.findElement(By.xpath("//button[text()='Allow All']")).click();
+//        System.out.println(driver.getWindowHandle());
+//        WebElement dr = driver.findElement(By.xpath("//div[@id='homepage-generator']//iframe"));
+//        System.out.println(String.valueOf(dr));
+//        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(dr));
+////        System.out.println(driver.getWindowHandle());
+////        driver.switchTo().window(String.valueOf(dr));
+//        System.out.println(driver.getWindowHandle());
+////        WebElement generate = driver.findElement(By.xpath("//div[@id='homepage-generator']//iframe"));
+////        System.out.println("lllllll" + generate.getText());
+////        wait.until(ExpectedConditions.elementToBeClickable(generate));
+//        driver.findElement(By.xpath("//input[@id='geevopfmhv-button']")).click();
+
+    }
+//    @AfterAll
+//    static void tearDown(){
+//        driver.quit();
+//    }
+
+
+}
